a03fa02001e3f756f761dc0e347f0e1d506adf8e
Mathias Bohm <mathias.bohm@cornelsen-services.de>
2016-06-27 14:33:54 +0200

    PaintCommand-SST hinzugefügt

---
+++ Samples/PizzaBot/Controllers/PaintCommandsController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Microsoft.Bot.Sample.PizzaBot.Controllers
+{
+    public class PaintCommandsController : ApiController
+    {
+        // GET: api/PaintCommands
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/PaintCommands/5
+        public string Get(int id)
+        {
+            return "value " + id;
+        }
+
+    }
+}
